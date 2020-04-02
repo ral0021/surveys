90b2d0ed0ac4a03d990de24d1d69680eb4cabfd0
superseeker13 <cardseller2@gmail.com>
2020-04-02 19:39:26 -0400

    fact currently works for (n -- (n-1)!)

--- survey_one/Historical/survey_one.fs
+++ survey_one/Historical/survey_one.fs
@@ -2,21 +2,23 @@
 \ Brackets are used instead of parans due to FORTH comment conventions
 \ To run: gforth survey_one.fs -e bye
 
-: double ( n -- n^2 )
-  2 * ;
+\Helper words
+: stkln
+    .s 10 EMIT ;
 
-: catalan ( n -- [1 / [n + 1]] * 2n!/[n![2n-n]]!)
+\Catalan
+: cat ( n -- [1 / [n + 1]] * 2n!/[n![2n-n]]!)
     ;
   
-: binomial ( n,k -- n!/[k![n-k]]!) 
+: bin ( n,k -- n!/[k![n-k]]!) 
     ;
 
-: factorial ( n -- n!) \ Brackets are used 
-    dup 1 - 
-    2 < if exit 
-        else recurse * 
-        then ;    
+: fact ( n -- n!) \ Brackets are used 
+    DUP            \ Currently (n -- (n-1)!)
+    1 -
+    DUP
+    2 > IF recurse * THEN EXIT ;
   
-10 0 do i 
+10 0 DO i 
 
     .s ;
