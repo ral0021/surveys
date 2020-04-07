657b6247b3f96ef4cc43980e7d93194d0eb67748
Edward <cardseller2@gmail.com>
2020-04-07 18:35:01 -0400

    Binomial structure

--- survey_one/Historical/survey_one.fs
+++ survey_one/Historical/survey_one.fs
@@ -9,11 +9,26 @@
 
 \Catalan
 : cat ( n -- [1 / [n + 1]] * 2n!/[n![2n-n]]!)
-    ;
-  
-: bin ( n,k -- n!/[k![n-k]]!) 
+    DUP
+    VARIABLE N
+    N !
+    1e SWAP /
     ;
 
+\Binomial    
+: bin ( n, k -- n!/[k![n-k]]!) 
+    VARIABLE N
+    N !
+    VARIABLE K
+    K !
+    N @ K @ -
+    fact
+    K @ fact
+    N @ fact
+    /
+    ;
+
+\Factorial
 : fact ( n -- n!) \ Brackets are used 
     DUP            \ Currently (n -- (n-1)!)
     1 -
